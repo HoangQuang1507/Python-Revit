f2a9fb8b89b9af55470dc7f20db4f03354882f38
HoangQuang1507 <93910675+HoangQuang1507@users.noreply.github.com>
2021-11-19 16:56:59 +0700

    upload

--- My_Python.tab/Select.panel/Select1.stack/Select File Link.pushbutton/script.py
+++ My_Python.tab/Select.panel/Select1.stack/Select File Link.pushbutton/script.py
@@ -12,6 +12,7 @@ doc = uidoc.Document
 
 collectorLink = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_RvtLinks).OfClass(RevitLinkInstance)
 listElementId = List[ElementId]()
+
 components = [Label('File Link:'), Separator()]
 for linkFile in collectorLink:
     newKey = "R" + linkFile.Id.ToString()
@@ -20,6 +21,7 @@ components.append(Separator())
 components.append(Button('Select'))
 ff = FlexForm("Hide Or UnHide Links", components)
 ff.show()
+
 #get value from check box
 result = ff.values
 listKey = result.keys()
--- My_Python.tab/Wall.panel/Wall 1.stack/Link Multiple DWG.pushbutton/script.py
+++ My_Python.tab/Wall.panel/Wall 1.stack/Link Multiple DWG.pushbutton/script.py
@@ -1,7 +1,36 @@
 # -*- coding: utf-8 -*-
-
-
+from Autodesk.Revit.DB import FilteredElementCollector,BuiltInCategory,ElementCategoryFilter 
+import os
 from rpw.ui.forms import select_file
 import pathlib
-path_file = select_file('Revit Model(*.dwg)|*.dwg',multiple = True)
-print(path_file)
+#get UIDocument
+uidoc = __revit__.ActiveUIDocument
+doc = uidoc.Document
+path_file = select_file('File Cad(*.dwg)|*.dwg',multiple = True)
+#print(path_file)
+listNameDwg = []
+nlDwg = []
+levelfileDwg = []
+
+for fileName in path_file:
+    fname = str(fileName).split('\\')[-1:][0]
+    listNameDwg.insert(0,fname)
+
+for levelDwg in listNameDwg:
+    lname = str(levelDwg).split("_")[-1:][0]
+    nlDwg.insert(0,lname)
+
+for lDwg in nlDwg:
+    levelnameDwg = str(lDwg).split(".")[0:][0]
+    #print(levelnameDwg)
+    levelfileDwg.insert(0,levelnameDwg)
+levelfileDwg.reverse()
+#print(levelfileDwg)
+
+dictDwg = zip(path_file,levelfileDwg)
+#print(dictDwg)
+collector = FilteredElementCollector(doc)
+filter = ElementCategoryFilter(BuiltInCategory.OST_Views)
+#print(filter)
+view = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements()
+print(view)
